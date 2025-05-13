66d3a188a1d9179baac131bea9b25ab48dd39cbb
NicoooM <nmarsan33000@gmail.com>
2025-05-13 15:10:12 +0200

    feat: get all rooms route

--- src/adapters/api/controller/hello-world.controller.ts
+++ src/adapters/api/controller/hello-world.controller.ts
@@ -1,29 +1,16 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { HelloWorldUseCase } from '../../../core/usecases/hello-world.use-case';
 import { HelloWorldMapper } from '../mapper/hello-world.mapper';
 import { HelloWorldRequest } from '../request/hello-world.request';
-import { CreateRoomUseCase } from '../../../core/usecases/create-room.use-case';
-import { CreateRoomRequest } from '../request/create-room.request';
-import { Room } from '../../../core/domain/model/Room';
-import { RoomMapper } from '../mapper/room.mapper';
 
 @Controller()
 export class HelloWorldController {
-  constructor(
-    private readonly helloWorldUseCase: HelloWorldUseCase,
-    private readonly createRoomUseCase: CreateRoomUseCase,
-  ) {}
+  constructor(private readonly helloWorldUseCase: HelloWorldUseCase) {}
 
   @Get()
   getHello(@Query('name') name: string): string {
     const request: HelloWorldRequest = { name };
     const command = HelloWorldMapper.toDomain(request);
     return this.helloWorldUseCase.execute(command);
-  }
-
-  @Post('/room')
-  async createRoom(@Body() body: CreateRoomRequest): Promise<Room> {
-    const command = RoomMapper.toDomain(body);
-    return this.createRoomUseCase.execute(command);
   }
 }
---
+++ src/core/usecases/get-all-rooms.use-case.spec.ts
@@ -0,0 +1,40 @@
+import { GetAllRoomsUseCase } from './get-all-rooms.use-case';
+import { InMemoryRoomRepository } from '../../adapters/in-memory/in-memory-room.repository';
+import { RoomRepository } from '../domain/repository/room.repository';
+import { CreateRoomCommand, CreateRoomUseCase } from './create-room.use-case';
+
+describe('GetAllRoomsUseCase', () => {
+  let roomRepository: RoomRepository;
+  let getAllRoomsUseCase: GetAllRoomsUseCase;
+  let createRoomUseCase: CreateRoomUseCase;
+
+  beforeEach(() => {
+    roomRepository = new InMemoryRoomRepository();
+    createRoomUseCase = new CreateRoomUseCase(roomRepository);
+    getAllRoomsUseCase = new GetAllRoomsUseCase(roomRepository);
+  });
+
+  it('should be defined', () => {
+    expect(getAllRoomsUseCase).toBeDefined();
+  });
+
+  it('should return all rooms', async () => {
+    // Given
+    const command: CreateRoomCommand = {
+      name: 'Koln',
+      capacity: 10,
+    };
+
+    // When
+    const room = await createRoomUseCase.execute(command);
+
+    expect(room).toBeDefined();
+
+    // When
+    const result = await getAllRoomsUseCase.execute();
+
+    // Then
+    expect(result.length).toEqual(1);
+    expect(result).toEqual([expect.objectContaining(room)]);
+  });
+});

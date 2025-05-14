28350e3ca29ef350af600fb3878ac65113a47216
NicoooM <nmarsan33000@gmail.com>
2025-05-14 10:06:55 +0200

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DomainErrorFilter } from './config/domain-error.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new DomainErrorFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  app.use(
+    '/reference',
+    apiReference({
+      spec: {
+        content: document,
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
